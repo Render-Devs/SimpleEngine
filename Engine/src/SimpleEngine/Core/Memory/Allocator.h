15ec1aca1d52ae89a0f537a1bf0475500ed2fbce
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2020-04-19 17:36:03 +0300

    Add align function to the base class.

--- Engine/src/SimpleEngine/Core/Memory/Allocator.h
+++ Engine/src/SimpleEngine/Core/Memory/Allocator.h
@@ -4,6 +4,8 @@
 
 namespace SimpleEngine::Core::Memory
 {
+    using word_t = intptr_t;
+
     class Allocator
     {
     protected:
@@ -17,6 +19,12 @@ namespace SimpleEngine::Core::Memory
 
         virtual void Init() = 0;
         virtual void* Allocate(const std::size_t chunkSize, const std::size_t alignment = 0) = 0;
-        virtual void Free(void* ptr) = 0;        
+        virtual void Free(void* ptr) = 0;
+
+    protected:
+        static size_t align(size_t n)
+        {            
+            return (n + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);
+        }
     };
 }
